dfa35bec753cee902d25b55337dd1b03f2b61432
Pavel <pas-zhukov@yandex.ru>
2023-08-15 16:37:31 +0300

    add DialogFlow learning script.

--- dialogflow.py
+++ dialogflow.py
@@ -1,8 +1,10 @@
+import json
 import logging
 
 from google.cloud import dialogflow
 from google.cloud import api_keys_v2
 from google.cloud.api_keys_v2 import Key
+import requests
 
 logger = logging.getLogger("DialogFlow")
 
@@ -41,7 +43,7 @@ def detect_intent_texts(project_id: str,
                         session_id: str or int,
                         text: str,
                         language_code: str = "ru-RU") -> str:
-    """Returns the result of detect intent with texts as inputs.
+    """Returns the result of detect intent with text as input.
 
     Args:
         project_id (str): ID of your DialogFlow project
@@ -75,5 +77,41 @@ def detect_intent_texts(project_id: str,
     return response.query_result.fulfillment_text
 
 
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
 if __name__ == "__main__":
-    detect_intent_texts("support-bot-devman", "test", "привет железяка", "ru-RU")
+    training_json_url = 'https://dvmn.org/media/filer_public/a7/db/a7db66c0-1259-4dac-9726-2d1fa9c44f20/questions.json'
+    response = requests.get(training_json_url)
+    response.raise_for_status()
+    training_questions = json.loads(response.content)
+
+    for topic in training_questions.keys():
+        questions = training_questions[topic]['questions']
+        answer = training_questions[topic]['answer']
+        create_intent('support-bot-devman', topic, questions, [answer])
+
+

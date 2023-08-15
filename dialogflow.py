5413e30f60224a495d0640d03c7ed9922763a51b
Pavel <pas-zhukov@yandex.ru>
2023-08-15 17:33:42 +0300

    format intent loading script

--- dialogflow.py
+++ dialogflow.py
@@ -77,30 +77,33 @@ def detect_intent_texts(project_id: str,
     return response.query_result.fulfillment_text
 
 
-def create_intent(project_id, display_name, training_phrases_parts, message_texts):
-    """Create an intent of the given intent type."""
+def create_intent(project_id: str,
+                  display_name: str,
+                  training_phrases_parts: list[str],
+                  message_texts: list[str]):
+    """Create an intent of the given intent type.
+
+    Args:
+        project_id (str): ID of your DialogFlow project
+        display_name (str): The Name of Intent
+        training_phrases_parts (list[str]): List of phrases you expect from users
+        message_texts (list[str]): List of answers for this intent
+
+    """
     intents_client = dialogflow.IntentsClient()
 
     parent = dialogflow.AgentsClient.agent_path(project_id)
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
         part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
-        # Here we create a new training phrase for each provided part.
         training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
         training_phrases.append(training_phrase)
-
     text = dialogflow.Intent.Message.Text(text=message_texts)
     message = dialogflow.Intent.Message(text=text)
 
-    intent = dialogflow.Intent(
-        display_name=display_name, training_phrases=training_phrases, messages=[message]
-    )
-
-    response = intents_client.create_intent(
-        request={"parent": parent, "intent": intent}
-    )
-
-    print("Intent created: {}".format(response))
+    intent = dialogflow.Intent(display_name=display_name, training_phrases=training_phrases, messages=[message])
+    response = intents_client.create_intent(request={"parent": parent, "intent": intent})
+    logger.debug("Intent created: {}".format(response))
 
 
 if __name__ == "__main__":
@@ -113,5 +116,3 @@ if __name__ == "__main__":
         questions = training_questions[topic]['questions']
         answer = training_questions[topic]['answer']
         create_intent('support-bot-devman', topic, questions, [answer])
-
-
